570ed69d7bdeaff6b7841e32fcd5d43976d72684
JOHANN MORA BERRIO <johannmora305074@correo.itm.edu.co>
2024-11-26 22:00:33 -0500

    Avance 2

---
+++ DAL/SeederDB.cs
@@ -0,0 +1,48 @@
+﻿using WebAPITest.DAL;
+
+namespace WEP_API_TEST.DAL
+{
+    public class SeederDB
+    {
+        private readonly DataBaseContext _context;
+
+        public SeederDB(DataBaseContext context)
+        {
+            context = _context;
+        }
+
+        //Crearemos un metodo llamado SeederAsync()
+        //Este metodo es un MAIN() 
+        //Este metodo tendra la responsabilidad de prepoblar diferentes tablas de la BD
+
+        public async Task SeederAsync()
+        {
+            // Primero; agregare un metodo propio de EF que hace las veces del comando  'update-database'
+            // En otras palabras: un metodo que me creara la BD inmediatamente ponga en ejecucion mi API
+            await _context.Database.EnsureCreatedAsync();
+
+            //Apartir de aqui vamos a ir creando metodos que me sirvan para prepoblar mi BD
+            await PopulateCountriesAsync();
+
+            await _context.SaveChangesAsync();
+
+        }
+
+
+
+        #region Private Methods 
+
+        private async Task PopulateCountriesAsync()
+        {
+            //El metodo Any negado (!) me indica que no hay nda absolutamente en la tabla 
+            //EL metodo !Any() me indica si la tabla Countries tiene almenos un registro
+
+            if (!_context.Countries.Any()) 
+            {
+
+            }
+        }
+
+        #endregion
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using WebAPITest.DAL;
 using WebAPITest.Domain.Interfaces;
 using WebAPITest.Domain.Services;
+using WEP_API_TEST.DAL;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,8 +18,23 @@ builder.Services.AddScoped<ICountryService, CountryService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<SeederDB>();
+
 
 var app = builder.Build();
+
+SeederData();
+
+void SeederData()
+{
+    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (IServiceScope? scope = scopedFactory.CreateScope())
+    {
+        SeederDB? service = scope.ServiceProvider.GetService<SeederDB>();
+        service.SeederAsync().Wait();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
